d664e88fc7fd3ec99b656c72bd9e7f03efe08e3a
Joshua Arnett <Josharnett850@gmail.com>
2018-03-31 21:11:59 -0400

    adding pickCharacter function and addToEnemy function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
     console.log("Document Ready");
+    let guileHealth = 130;
+    let kenHealth = 150;
+    let blankaHealth = 110;
+    let ryuHealth = 180;
+    $("#kenHealth").html(kenHealth);
+    $("#ryuHealth").html(ryuHealth);
+    $("#blankaHealth").html(blankaHealth);
+    $("#guileHealth").html(guileHealth);
+
+    const pickCharacter = function() {
+        let characterArr = ["#ken", "#ryu", "blanka", "guile"];
+        $("button").on("click", function() {
+            if(this.id === "ken") {
+                $("#kenContainer").appendTo("#yourCharacter");
+                characterArr.splice(0, 1);
+                console.log(characterArr);
+            } else if(this.id ==="ryu") {
+                $("#ryuContainer").appendTo("#yourCharacter");
+            } else if(this.id ==="guile") {
+                $("#guileContainer").appendTo("#yourCharacter");
+            } else if(this.id ==="blanka") {
+                $("#blankaContainer").appendTo("#yourCharacter");
+            }
+
+            const addToEnemy = function(arr) {
+
+            } 
+        });
+    }
+    pickCharacter();
+
+ 
 });
